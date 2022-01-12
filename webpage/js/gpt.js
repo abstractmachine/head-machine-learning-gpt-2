14d78bf10c563578b0837c6b7b850071003625c6
Douglas Edric Stanley <douglas@abstractmachine.net>
2022-01-12 12:14:36 +0100

    Added working training + server + webpage demo

--- webpage/js/gpt.js
+++ webpage/js/gpt.js
@@ -1,18 +1,20 @@
 
 async function talkToRobot() {
 
-	const prompt = "HAMLET:"
+	// ge
+	let prompt = document.getElementById('input').value
+
+	console.log(prompt)
+	
 	// Create WebSocket connection.
-	const request = await fetch("http://localhost:8000/ask;" + prompt, {
+	const request = await fetch("http://localhost:8000/ask;'" + prompt + "'", {
 		method: 'GET'
 	})
 
-	output = document.getElementById('output')
-
-	output.textContent = 'sending request'
+	document.getElementById('output').textContent = 'sending request'
 
 	const value = await request.text()
 	
-	output.textContent = value
+	document.getElementById('output').textContent = value
 
 }
